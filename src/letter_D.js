9d36e2ba61fe4b1abfab1fa035114eeb9d710c9f
Nodir Malikov <nodir@mal1kov.uz>
2023-12-20 14:35:17 +0500

    upd

--- src/letter_D.js
+++ src/letter_D.js
@@ -1,5 +1,3 @@
-import * as THREE from '../node_modules/three/src/Three.js';
-
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000); // черный фон
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
