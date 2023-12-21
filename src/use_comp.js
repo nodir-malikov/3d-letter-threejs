f7d3f034f46e1a379f8442f0cf9c08ac4cc91726
Nodir Malikov <nodir@mal1kov.uz>
2023-12-21 19:39:14 +0500

    Add use_comp.js script to HTML files

---
+++ src/use_comp.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		alert("Для лучшего опыта пожалуйста используйте этот сайт только на компьютере.");
+	}
+});
